8447281df5e59b63dbb924a569112c1771291941
Pedro Lucas <pedroldl123@gmail.com>
2024-07-11 15:10:47 -0300

    header component, drizzle orm & neon database

--- components/header.tsx
+++ components/header.tsx
@@ -1,4 +1,8 @@
+import { UserButton, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
 import { HeaderLogo } from "./header-logo";
+import { Navigation } from "./navigation";
+import { Loader2 } from "lucide-react";
+import { WelcomeMsg } from "./welcome-msg";
 
 export function Header() {
 	return (
@@ -7,8 +11,16 @@ export function Header() {
 				<div className="w-full flex items-center justify-between mb-14">
 					<div className="flex items-center lg:gap-x-16">
 						<HeaderLogo />
+						<Navigation />
 					</div>
+					<ClerkLoaded>
+						<UserButton afterSwitchSessionUrl="/" />
+					</ClerkLoaded>
+					<ClerkLoading>
+						<Loader2 className="animate-spin size-8 text-slate-400" />
+					</ClerkLoading>
 				</div>
+				<WelcomeMsg />
 			</div>
 		</header>
 	)
